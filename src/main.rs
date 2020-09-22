26cbfc178f96f0441257c60f2122e8e7746c33ec
Dave Smith <dave@sophoservices.com>
2020-09-21 21:28:46 -0500

    Split server code into separate file

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::net::TcpListener;
 use std::thread::spawn;
 use tungstenite::server::accept;
 use std::sync::mpsc;
-use std::sync::Arc;
 use std::time::Duration;
+
+mod server;
 
 fn main() {
     let server = TcpListener::bind("127.0.0.1:9001").unwrap();
@@ -15,52 +16,32 @@ fn main() {
     //    let outgoing_list: Arc<Vec<mpsc::Sender<&str>>> = Arc::new(Vec::new());
 
     // let central_outgoing = outgoing_list.clone();
-    spawn (move || {
-	let central_outgoing = Vec::new()
-	loop {
-	    match rx.try_recv() {
-		Ok(recv_msg) => {
-		    println!("{}", msg);
-		    for tx in *central_outgoing {
-			tx.send(msg).unwrap();
-		    }
-		},
-		Err(mpsc::TryRecvError::Empty) => ,
-		Err(mpsc::TryRecvError::Disconnected) => println!("central recv disconnected"),
-	    }
-
-	    // any new transmit clients
-	    match newch_rx.try_recv() {
-		Ok(new_channel) => central_outgoing.push(new_channel),
-		Err(mpsc::TryRecvError::Empty) => ,
-		Err(mpsc::TryRecvError::Disconnected) => println!("new channel recv disconnected"),
-	    }
-
-	    std::thread::sleep(Duration::new(0,100));
-	}
-	
-    });
+    spawn (move || server::core(rx, newch_rx));
     
     for stream in server.incoming() {
 	let tx_clone = tx.clone();
 	let (tx2, rx2) = mpsc::channel();
-	newch_tx
-	    
+	newch_tx.send(tx2).unwrap();
+	
 	spawn (move || {
 	    let mut websocket = accept(stream.unwrap()).unwrap();
-	    websocket.get_ref().set_read_timeout(Some(Duration::new(0,100)));
+	    websocket.get_ref().set_read_timeout(Some(Duration::new(0,100))).unwrap();
 	    
 	    loop {
 		let msg_res = websocket.read_message();
 		match msg_res {
-		    Ok(msg) => tx_clone.send(&msg.into_text().unwrap()).unwrap(),
+		    Ok(msg) => {
+//			let msg: &str = 
+			tx_clone.send(msg).unwrap()
+		    },
 		    Err(x) => println!("websocket error: {}", x),
+		    // TODO handle IO error: Resource temporarily unavailable - we're using non-blocking reads
 		}
 		// check rx2 for messages too
 		let recv_res = rx2.try_recv();
 		match recv_res {
-		    Ok(recv_msg) => websocket.write_message(tungstenite::Message::text(recv_msg)).unwrap(),
-		    Err(mpsc::TryRecvError::Empty) => println!("rx2 empty"),
+		    Ok(recv_msg) => websocket.write_message(recv_msg).unwrap(),
+		    Err(mpsc::TryRecvError::Empty) => (),
 		    Err(mpsc::TryRecvError::Disconnected) => println!("rx2 disconnect"),
 		}
 		// send it to the central thread
---
+++ src/server.rs
@@ -0,0 +1,28 @@
+use std::sync::mpsc;
+use std::time::Duration;
+
+pub fn core (rx: mpsc::Receiver<tungstenite::Message>, newch_rx: mpsc::Receiver<mpsc::Sender<tungstenite::Message>>) {
+    let mut central_outgoing: Vec<mpsc::Sender<tungstenite::Message>> = Vec::new();
+    loop {
+	match rx.try_recv() {
+	    Ok(recv_msg) => {
+		println!("{}", recv_msg);
+		for tx in &central_outgoing {
+		    tx.send(recv_msg.clone()).unwrap();
+		}
+	    },
+	    Err(mpsc::TryRecvError::Empty) => (),
+	    Err(mpsc::TryRecvError::Disconnected) => println!("central recv disconnected"),
+	}
+
+	// any new transmit clients
+	match newch_rx.try_recv() {
+	    Ok(new_channel) => central_outgoing.push(new_channel),
+	    Err(mpsc::TryRecvError::Empty) => (),
+	    Err(mpsc::TryRecvError::Disconnected) => println!("new channel recv disconnected"),
+	}
+
+	std::thread::sleep(Duration::new(0,100));
+    }
+    
+}
