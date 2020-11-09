025ecdf3d6df5b85ff99d33ec1537d424fdb21e2
Dave Smith <dave@sophoservices.com>
2020-11-09 06:04:03 -0600

    Ability to pass back the URI that was connected to from the websocket
    Also will give us the ability to grab cookies, etc.

--- src/websocket_headers.rs
+++ src/websocket_headers.rs
@@ -1,19 +1,19 @@
 use tungstenite::handshake::server::{Callback,ErrorResponse,Request,Response};
-use std::sync::Arc;
-use log::debug;
+use std::sync::{Arc,RwLock};
+use log::{debug,error};
 #[derive(Debug)]
 pub struct WebsocketHeaders {
     pub uri: Option<http::uri::Uri>,
 }
 
 pub struct WebsocketHeadersCB {
-    data: Arc<WebsocketHeaders>,
+    data: Arc<RwLock<WebsocketHeaders>>,
 }
 
 impl Callback for WebsocketHeadersCB {
     fn on_request(self, request: &Request, response: Response)
 		  -> Result<Response, ErrorResponse> {
-	self.data.borrow().set_uri(request.uri().clone()); 
+	self.set_uri(request.uri().clone());
 	debug!("URI: {}", request.uri());
 	Ok(response)
     }
@@ -21,18 +21,21 @@ impl Callback for WebsocketHeadersCB {
 
 pub fn new_callback() -> WebsocketHeadersCB {
     WebsocketHeadersCB {
-	data: Arc::new(
+	data: Arc::new(RwLock::new(
 	    WebsocketHeaders {
 		uri: None,
-	    })
+	    }))
     }
 }
 
 impl WebsocketHeadersCB {
-    pub fn hdr(&self) -> Arc<WebsocketHeaders> {
+    pub fn hdr(&self) -> Arc<RwLock<WebsocketHeaders>> {
 	self.data.clone()
     }
     pub fn set_uri(&self, uri:http::uri::Uri) {
-	self.data.uri = Some(uri);
+	match self.data.write() {
+	    Ok(mut x) => (*x).uri = Some(uri),
+	    Err(e) => error!("Unable to lock websocket header struct: {}", e),
+	}
     }
 }
