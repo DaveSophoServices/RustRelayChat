0ceec2db3f5f1a72bbacbc8f2ef797d0d6bf65d5
Dave Smith <dave@sophoservices.com>
2020-11-12 07:03:34 -0600

    WIP: Trying to instantiate ChannelServer into a thread

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use std::sync::{Arc,RwLock};
 use std::collections::HashMap;
+use std::sync::mpsc;
 use log::{debug};
 
 mod channel_server;
@@ -8,16 +9,20 @@ use super::websocket_headers::WebsocketHeaders;
 
 #[derive(Clone)]
 pub struct Server {
-    channels: Arc<HashMap<String,ChannelServer>>,
     shutdown: Arc<RwLock<u32>>,
 }
 
+struct ChannelList {
+    list: HashMap<String,(Arc<ChannelServer>, Arc<mpsc::Sender<tungstenite::Message>>)>,
+}
+
+static CHANNELS: RwLock<ChannelList> = RwLock::new(
+    ChannelList { list: HashMap::new(), });
 
 // Server holds a map of channel strings to channel servers
 impl Server {
     pub fn new() -> Server {
 	Server {
-	    channels: Arc<HashMap::new()>,
 	    shutdown: Arc::new(RwLock::new(0)),
 	}
     }
@@ -26,17 +31,30 @@ impl Server {
 	self.shutdown.clone()
     }
     
-    pub fn get(&self, ws_hdr:Arc<RwLock<WebsocketHeaders>>) -> &ChannelServer{
+    pub fn get(&self, ws_hdr:Arc<RwLock<WebsocketHeaders>>) -> Arc<ChannelServer>{
 	let uri = match ws_hdr.read().unwrap().uri {
 	    Some(x) => x.to_string(),
 	    None => String::from(""),
+	};
+	let mut ret: Option<&Arc<ChannelServer>>;
+	let tx: &Arc<mpsc::Sender<tungstenite::Message>>;
+	if let Ok(channels) = CHANNELS.read() {
+	    if let Some((r,t)) = channels.list.get(&uri) {
+		ret = Some(r);
+		tx = t;
+	    }
 	}
-	match self.channels.get(&uri) {
-	    Some(x) => x,
+	match ret {
+	    Some(x) => x.clone(),
 	    None => {
 		debug!("Creating new channel_server: {}", uri);
-		self.channels.insert(uri, channel_server::new());
-		self.channels.get(&uri).unwrap()
+		match CHANNELS.write() {
+		    Ok(channels) => {
+			channels.list.insert(uri, channel_server::new(self.shutdown.clone(), uri));
+			channels.list.get(&uri).unwrap().0
+		    },
+		    Err(_) => panic!("server::get : CHANNELS RwLock failed to write"),
+		}
 	    }
 	}
     }
--- src/server/channel_server.rs
+++ src/server/channel_server.rs
@@ -2,53 +2,54 @@ use std::sync::{mpsc,Arc,RwLock};
 use std::time::Duration;
 
 use std::thread::spawn;
-use log::debug;
+use log::{debug,info,warn};
 use tungstenite::Message;
 use crate::stats::Stats;
 
 pub struct ChannelServer {
-    central_outgoing: Vec<mpsc::Sender<Message>>,
-    tx: mpsc::Sender<Message>,
-    rx: mpsc::Receiver<Message>,
-    newch_rx: mpsc::Receiver<mpsc::Sender<Message>>,
-    shutdown: Arc<RwLock<i32>>,
+    central_outgoing: RwLock<Vec<Arc<mpsc::Sender<Message>>>>,
+    shutdown: Arc<RwLock<u32>>,
     stats: Arc<Stats>,
     channel: String,
 }
 
-impl ChannelServer {
-    pub fn new(	   newch_rx: mpsc::Receiver<mpsc::Sender<tungstenite::Message>>,
-	       shutdown:std::sync::Arc<std::sync::RwLock<i32>>,
-	       channel:String,
-    ) -> ChannelServer {
-	let (tx,rx) = mpsc::channel();
-	ChannelServer {
-	    central_outgoing: Vec::new(),
-	    tx,
-	    rx,
-	    newch_rx,
-	    shutdown,
-	    stats: Arc::new(Stats::new()),
-	    channel,
-	}
+pub fn new(shutdown:std::sync::Arc<std::sync::RwLock<u32>>,
+	   channel:String,
+) -> (Arc<ChannelServer>,Arc<mpsc::Sender<Message>>) {
+    // core's rx and tx
+    let (tx,rx) = mpsc::channel();
+
+    let ret = Arc::new(ChannelServer {
+	central_outgoing: RwLock::new(Vec::new()),
+	shutdown,
+	stats: Arc::new(Stats::new()),
+	channel,
+    });
+    
+    {
+	let ret = ret.clone();
+	spawn(move || ret.core(rx) );
     }
+    return (ret,Arc::new(tx));
+}
+impl ChannelServer {
     pub fn get_stats(&self) -> Arc<Stats> {
 	return self.stats.clone();
-    }
-    pub fn spawn_core(mut self) {
-    	spawn( move || self.core() );
     }
     pub fn get_tx_rx(&self) -> (mpsc::Sender<tungstenite::Message>,
 				mpsc::Receiver<tungstenite::Message>)
     {
 	// this pair lets the websocket client receive from us. We return the rx to them, keeping tx in our list
 	let (tx,rx) = mpsc::channel();
-	self.central_outgoing.push(tx);
+	if let Ok(co_write) = self.central_outgoing.write() {
+	    co_write.push(tx);
+	    self.stats.set_num_clients(co_write.len());
+	}
 
 	// clone the transmit end of our receiver so that clients can send to us
-	(self.tx.clone(), rx)
+	(self.tx.read().unwrap().clone(), rx)
     }
-    pub fn core (&mut self) {
+    pub fn core (&mut self, rx: mpsc::Receiver<Message>) {
 	// we may want to remove
 	let mut channels_to_be_removed = Vec::new();
 
@@ -59,12 +60,12 @@ impl ChannelServer {
 	    }
 	    let mut done_something = false;
 	    
-	    match self.rx.try_recv() {
+	    match self.rx.read().unwrap().try_recv() {
 		Ok(recv_msg) => {
 		    debug!("* {}", recv_msg);
 
 		    info!("* Sending msg '{}' to {} channels", recv_msg, self.stats.num_clients());
-		    for (i,tx) in self.central_outgoing.iter().enumerate() {
+		    for (i,tx) in self.central_outgoing.read().unwrap().iter().enumerate() {
 			match tx.send(recv_msg.clone()) {
 			    Ok(_) => (),
 			    Err(_) => {
@@ -74,32 +75,23 @@ impl ChannelServer {
 			}
 		    }
 		    if ! channels_to_be_removed.is_empty() {
-			loop {
-			    match channels_to_be_removed.pop() {
-				Some(x) => {
-				    debug!("Dropping tx channel");
-				    self.central_outgoing.remove(x);
-				},
-				None => break, // from loop
-			    };
+			if let Ok(co_write) = self.central_outgoing.write() {
+			    loop {
+				match channels_to_be_removed.pop() {
+				    Some(x) => {
+					debug!("Dropping tx channel");
+					co_write.remove(x);
+				    },
+				    None => break, // from loop
+				};
+			    }
+			    self.stats.set_num_clients(co_write.len());
 			}
 		    }
 		    done_something = true;
 		},
 		Err(mpsc::TryRecvError::Empty) => (),
 		Err(mpsc::TryRecvError::Disconnected) => warn!("central recv disconnected - all the clients gone?"),
-	    }
-
-	    // any new transmit clients
-	    match self.newch_rx.try_recv() {
-		Ok(new_channel) => {
-		    self.central_outgoing.push(new_channel);
-		    self.stats.set_num_clients(self.central_outgoing.len());
-		    //println!("* received a send channel");
-		    done_something = true;
-		},
-		Err(mpsc::TryRecvError::Empty) => (),
-		Err(mpsc::TryRecvError::Disconnected) => debug!("new channel recv disconnected"),
 	    }
 
 	    if !done_something {
