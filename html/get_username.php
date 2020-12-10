544dc71118790e966526ca20a8d3935f1ec9cc32
Dave Smith <dave@sophoservices.com>
2020-12-10 16:04:43 -0600

    Wordpress component to return the username and password

---
+++ html/get_username.php
@@ -0,0 +1,19 @@
+<?php
+// https://wordpress.stackexchange.com/questions/76465/initialize-wordpress-environment-to-use-in-command-line-script
+
+define( 'BASE_PATH', __DIR__ . "/" );
+define('WP_USE_THEMES', false);
+global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header;
+
+require_once(BASE_PATH . '/wp-load.php');
+$user = wp_get_current_user();
+
+if ($user->exists()) {
+	$ret["login"] = $user->user_login;
+	$ret["email"] = $user->user_email;
+	$ret["display"] = $user->display_name;
+} else {
+	$ret["err"] = 'user not logged in';
+}
+echo json_encode($ret);
+?>
