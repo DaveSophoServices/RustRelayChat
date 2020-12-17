01d4e79deece3459f82d405d3a211234e7612ec1
Dave Smith <dave@sophoservices.com>
2020-12-17 17:01:19 -0600

    Try a different HMAC library

--- src/hasher.rs
+++ src/hasher.rs
@@ -1,18 +1,24 @@
-use sha2::Sha256;
-use hmac::Hmac;
-
-type HmacSha256 = Hmac<Sha256>;
+use hmac_sha256;
 
 pub fn verify(code: &str, body:&str, key: &str) -> Result<bool,_> {
 
     // parse a[0] into a byte array
-    let code = hex::decode(code);
+    let code = match hex::decode(code) {
+        Ok(x) => x,
+        Err(e) => {
+            return Err(format!("Failed to decode sig on verify command: {}", e));
+        },
+    };
     
-    // initialize hmac code with secret key
-    let mut hmac = HmacSha256::new_varkey(b"mysecretkey");
+    // TODO convert vec to byte[]
+    // https://stackoverflow.com/questions/29570607/is-there-a-good-way-to-convert-a-vect-to-an-array
+    
+    // initialize and obtain hmac code with secret key
+    let mut hmac = hmac_sha256::HMAC::mac(code, key.as_bytes());
     
     // verify body
     // https://docs.rs/hmac/0.10.1/hmac/
-    hmac.update(body);
-    hmac.verify(&code)
+    /* hmac.update(body);
+    hmac.verify(&code) */
+    Ok(true)
 }
