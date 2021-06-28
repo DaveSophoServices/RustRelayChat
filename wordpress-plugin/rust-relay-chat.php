b1fc1d88ce44766c5921d4fe9af85065adc966d4
Dave Smith <dave@sophoservices.com>
2021-06-28 13:15:50 -0500

    Add base for wordpress plugin

---
+++ wordpress-plugin/rust-relay-chat.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Rust Relay Chat
+Description: A chat client and HTML chat form
+Author: Dave Smith
+*/
+
+
+require_once plugin_dir_path(__FILE__) . 'includes/functions.php'
+?>
